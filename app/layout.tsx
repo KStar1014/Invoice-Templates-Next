0f0bf704217a863e839fb561675b6c96443cc843
kstar1014 <washingtons1014@outlook.com>
2025-05-21 16:58:21 -0500

    update data

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Invoice Templates Website',
+  description: 'Design 6 modern invoice templates',
+  generator: 'Amel',
 }
 
 export default function RootLayout({
